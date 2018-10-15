86927944ddbd95b0620886d0981006373ede3069
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-10-15 22:46:22 +0600

    * Removed unused codes * Added example file

---
+++ src/examples/Example.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="example">
+        <h2 class="title">Example 1</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Example'
+}
+</script>
+
+<style scoped lang="scss">
+    .example {
+        .title {
+            text-align: center;
+        }
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Example from './examples/Example'
 
 Vue.config.productionTip = false
 
+const Comp = process.env['NODE_ENV'] === 'Example' ? Example : App
+
 new Vue({
-  render: h => h(App)
+  render: h => h(Comp)
 }).$mount('#app')
